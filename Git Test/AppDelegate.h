463cc5837fb79f1b6f7619a01ca1451d05bd9829
Sean Wells <seanwells@Seans-Mac-Pro-LS1.local>
2015-02-19 15:58:06 -0800

    Initial Commit

---
+++ Git Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Test
+//
+//  Created by Sean Wells on 2/19/15.
+//  Copyright (c) 2015 Vocal Swarm LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git Test
+//
+//  Created by Sean Wells on 2/19/15.
+//  Copyright (c) 2015 Vocal Swarm LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
