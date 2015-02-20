496eb5e35780384d88214a910e68f9315bea05cb
Sean Wells <seanwells@Seans-Mac-Pro-LS1.local>
2015-02-19 16:06:32 -0800

    added a test clas

---
+++ testTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  testTableViewController.h
+//  Git Test
+//
+//  Created by Sean Wells on 2/19/15.
+//  Copyright (c) 2015 Vocal Swarm LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface testTableViewController : UITableViewController
+
+@end
